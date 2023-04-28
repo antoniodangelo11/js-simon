8bd818012464ec41dc2c9edaec9f7c8945dfe3bc
antoninodangelo11 <reapercsp@gmail.com>
2023-04-28 10:02:38 +0200

    reset date

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@ Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina
 /*____________________________________________*/
 
 // Varibile dove indica la data a cui puntare
-const countDownDate = new Date("July 28, 2023 09:30:00").getTime();
+const countDownDate = new Date("April 28, 2023 09:30:00").getTime();
 
 // Aggiorna l'intervallo un secondo alla volta
 const setTimer = setInterval(function() {
@@ -18,17 +18,17 @@ const setTimer = setInterval(function() {
   let distance = countDownDate - currentDate;
     
   // Calcolo del tempo per giorni, ore, minuti e secondi
-  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
   // Stampo il risultato nel dom in tempo reale
   document.querySelector(".result").innerHTML = days + "G. " + hours + "Ore "
   + minutes + "Min. " + seconds + "Sec. ";
     
   // Quanto il conteggio finirà stampo un messaggio di conclusione
-  if (distance < 0) {
+  if (distance <= 0) {
     clearInterval(setTimer);
     document.querySelector(".result").innerHTML = "FINITO!";
   }
